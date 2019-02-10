5567f6f8c5e3d416397ea2b1e4d670d7134fc4c1
MinhazMurks <minhazabdullah1@gmail.com>
2019-02-10 16:24:31 -0500

    Added animation stuff
    Animation stuff added!

---
+++ Assets/Scripts/PlayAnim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnim : MonoBehaviour
+{
+    public Animator animation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/VidTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VidTrigger : MonoBehaviour
+{
+    public UnityEngine.Video.VideoPlayer video_player;
+
+    public UnityEditor.Animations.AnimatorController animator;
+
+    public int stop;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        video_player.playOnAwake = false;
+   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        video_player.Play();
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        vp.Stop();
+    }
+}
