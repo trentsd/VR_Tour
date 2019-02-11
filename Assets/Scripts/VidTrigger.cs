1bcc7eb5f056d04103715680b7f725c1fc8dac5b
MinhazMurks <minhazabdullah1@gmail.com>
2019-02-11 18:50:09 -0500

    Hub Walls

--- Assets/Scripts/VidTrigger.cs
+++ Assets/Scripts/VidTrigger.cs
@@ -9,11 +9,17 @@ public class VidTrigger : MonoBehaviour
     public int stop;
     public GameObject screen;
 
+    bool hasExited = false;
+
+
     void Start()
     {
         video_player.playOnAwake = false;
         animator.enabled = true;
-        animator.SetBool("bool", false);
+        animator.SetBool("play", false);
+        animator.SetBool("reverse", false);
+
+        hasExited = false;
 
         animator.GetComponent<Renderer>().enabled = false;
         screen.GetComponent<Renderer>().enabled = false;
@@ -21,21 +27,38 @@ public class VidTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        hasExited = false;
         animator.GetComponent<Renderer>().enabled = true;
         animStart();
         Invoke("playVid", 1f);
         
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        hasExited = true;
+        closeTv();
+    }
+
     void animStart()
     {
-        animator.SetBool("bool", true);
+        animator.SetBool("reverse", false);
+        animator.SetBool("play", true);
+    }
+
+    void animReverse()
+    {
+        animator.SetBool("reverse", true);
+        animator.SetBool("play", true);
     }
 
     void playVid()
     {
-        screen.GetComponent<Renderer>().enabled = true;
-        video_player.Play();
+        if(!hasExited)
+        {
+            screen.GetComponent<Renderer>().enabled = true;
+            video_player.Play();
+        }
     }
 
     void EndReached(UnityEngine.Video.VideoPlayer vp)
@@ -45,8 +68,18 @@ public class VidTrigger : MonoBehaviour
 
     void closeTv()
     {
-        animator.GetComponent<Renderer>().enabled = false;
+        video_player.Pause();
+        video_player.Stop();
         screen.GetComponent<Renderer>().enabled = false;
+        animReverse();
+        Invoke("vanishTv", 1f);
+    }
+
+    void vanishTv()
+    {
+        animator.GetComponent<Renderer>().enabled = false;
+        animator.SetBool("reverse", false);
+        animator.SetBool("play", false);
     }
 
 }
